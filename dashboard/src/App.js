d9c0aef86982afc3175bd093c7109664db0cffbf
Shawn Giroux <shawn.giroux90@gmail.com>
2018-12-31 10:46:17 -0500

    Added another icon, and fixed clicking bounds for links in sidebar

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Sidebar from './Components/sidebar/Sidebar.js'
+import Sidebar from './Components/sidebar/Sidebar.js';
 import About from './page2.js';
 import Home from './page1.js';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTachometerAlt } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTools } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faTachometerAlt)
+library.add(faTachometerAlt, faTools)
 
 class App extends Component {
   render() {
--- dashboard/src/Components/sidebar/Sidebar.js
+++ dashboard/src/Components/sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Sidebar.css';
 import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Sidebar extends React.Component {
   getNavLinkClass = (path) => {
@@ -17,11 +17,16 @@ class Sidebar extends React.Component {
           </div>
           <ul>
             <li className={this.getNavLinkClass("/")}>
-              <FontAwesomeIcon icon="tachometer-alt" size="lg" />
-              <NavLink to="/">DASHBOARD</NavLink>
+              <NavLink to="/">
+                <FontAwesomeIcon icon="tachometer-alt" size="md" />
+                Dashboard
+              </NavLink>
             </li>
             <li className={this.getNavLinkClass("/About")}>
-              <NavLink to="About">ABOUT</NavLink>
+              <NavLink to="About">
+                <FontAwesomeIcon icon="tools" size="md" />
+                Settings
+              </NavLink>
             </li>
           </ul>
       </div>
