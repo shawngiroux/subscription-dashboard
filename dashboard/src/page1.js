19aeba2aa1487444a57a8b3e452eb2839cee3f51
calico <shawn.giroux90@gmail.com>
2019-02-09 15:29:43 -0500

    Removing test pages

--- dashboard/src/page1.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export function Home(props) {
-  return (
-    <p>This is the home page</p>
-  );
-}
-
-export default Home;
--- dashboard/src/page2.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-function About(props) {
-    return (
-      <p>This is the about me page</p>
-    );
-}
-
-export default About;
