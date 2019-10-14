318c674a5b66f2fb85f1c90a982fa9d243005604
Shawn Giroux <shawn.giroux90@gmail.com>
2019-10-14 18:16:19 -0400

    Moving subscription table to manage subscriptions
    Right now, pages are just kind of shoehorned components. I will start
    changing these into components are sit in containers and container
    items (from a css standpoint).
    
    The subscription table will remain in the main page until I come up
    with content for that location.

--- src/Components/manage-subscriptions/manage-subscriptions.jsx
+++ src/Components/manage-subscriptions/manage-subscriptions.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
+import { SubscriptionTable } from "../subscription-table/Subscription-table.jsx";
 import "./manage-subscriptions.css";
 
 export class ManageSubscriptions extends Component {
     render() {
         return (
-            <div className="widget">
-                <div className="widget-header">
-                    <span>Manage Subscriptions</span>
+            <div className="container">
+                <div className="container-item">
+                    <SubscriptionTable/>
                 </div>
-                <div className="widget-body subscriptions">
-                    <div className="input-group">
-                        <span>Add Subscription</span>
-                        <input type="text"></input>
+                <div className="container-item">
+                    <div className="widget">
+                        <div className="widget-header">
+                            <span>Manage Subscriptions</span>
+                        </div>
+                        <div className="widget-body subscriptions">
+                            <div className="input-group">
+                                <span>Add Subscription</span>
+                                <input type="text"></input>
+                            </div>
+                            <button className="btn">Add Subscription</button>
+                        </div>
                     </div>
-                    <button className="btn">Add Subscription</button>
                 </div>
             </div>
         );
